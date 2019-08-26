8c6e3eeff7cf1c6ec658a42991516816ef038de0
hicucu <hicucu@gmail.com>
2019-08-27 00:51:53 +0900

    Updates

---
+++ precache-manifest.35f4add776a0083a8f2cc60de02ff225.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f1989fd554191c88d9928c76ea45c1",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "1b3f748cf2e7f6b0c1c5",
+    "url": "/movie_app_2019/static/css/main.adf50e78.chunk.css"
+  },
+  {
+    "revision": "3350fcf69a8fdf7d4a1c",
+    "url": "/movie_app_2019/static/js/2.1c4cb735.chunk.js"
+  },
+  {
+    "revision": "1b3f748cf2e7f6b0c1c5",
+    "url": "/movie_app_2019/static/js/main.6bb266b3.chunk.js"
+  },
+  {
+    "revision": "8e34f0f3f9415f236696",
+    "url": "/movie_app_2019/static/js/runtime~main.97c70ab9.js"
+  }
+]);
